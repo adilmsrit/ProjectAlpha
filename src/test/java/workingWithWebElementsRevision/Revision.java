a901f8cb76a823ab19a73c230fd3fed645b612ef
adilmsrit <adilmsrit@gmail.com>
2020-11-07 09:30:13 +0400

    Fixed a defect (#10)

--- src/test/java/workingWithWebElementsRevision/Revision.java
+++ src/test/java/workingWithWebElementsRevision/Revision.java
@@ -5,12 +5,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class Revesion extends Base {
+public class Revision extends Base {
 
-@Test
-    public void TestCase_1(){
-    
-}
+    @Test
+    public void TestCase_1() {
+        WebDriver driver = super.Base();
+        driver.get("http://www.yahoo.com");
+
+        //Adil has fixed a defect here.
+
+    }
 
 
 }
