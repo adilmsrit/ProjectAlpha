e78ae2b73d4534ed13cfbd8fc70bf2316da54911
adilmsrit <adilmsrit@gmail.com>
2020-11-07 09:10:17 +0400

    Adding new changes (#5)

---
+++ src/test/java/workingWithWebElementsRevision/Revision.java
@@ -0,0 +1,16 @@
+package workingWithWebElementsRevision;
+
+import Utilities.Base;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Revesion extends Base {
+
+@Test
+    public void TestCase_1(){
+    
+}
+
+
+}
