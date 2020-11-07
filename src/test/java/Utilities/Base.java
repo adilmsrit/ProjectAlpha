b3371632290ddae37f819ffd2186ba74fa3ce74b
adilmsrit <adilmsrit@gmail.com>
2020-11-07 09:40:00 +0400

    Release (#13)
    * Adding new changes (#5)
    
    * s (#11)
    
    * Adding new changes
    
    * Adding new changes (#5) (#6)
    
    * Adding new changes (#5) (#7)
    
    * Adding new changes (#5) (#8) (#9)
    
    * Fixed a defect (#10)
    
    * s (#12)
    
    * Adding new changes
    
    * Adding new changes (#5) (#6)
    
    * Adding new changes (#5) (#7)
    
    * Adding new changes (#5) (#8) (#9)
    
    * Fixed a defect (#10)

--- src/test/java/Utilities/Base.java
+++ src/test/java/Utilities/Base.java
@@ -1,2 +1,20 @@
-package Utilities;public class Base {
-}
+
+package Utilities;
+
+import eventFiringWebDriver.IamTheListnerClass;
+import eventFiringWebDriver.IamTheListnerClass2;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Base {
+    public WebDriver Base() {
+        WebDriver driver;
+        System.setProperty("webdriver.chrome.driver", "./src/test/resources/drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        return driver;
+    }
--- src/test/java/workingWithWebElementsRevision/Revision.java
+++ src/test/java/workingWithWebElementsRevision/Revision.java
@@ -5,12 +5,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class Revesion extends Base {
 
-@Test
-    public void TestCase_1(){
-    
-}
+public class Revision extends Base {
+
+    @Test
+    public void TestCase_1() {
+        WebDriver driver = super.Base();
+        driver.get("http://www.yahoo.com");
+
+        //Adil has fixed a defect here.
+
+    }
 
 
 }
